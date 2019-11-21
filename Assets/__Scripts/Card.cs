8c3c4053d54226ee1082321bc3911b33fad23891
jackly5154 <49957081+jackly5154@users.noreply.github.com>
2019-11-20 21:55:01 -0500

    Finished all scripts and things. Problems: 1) Faces of cards do not have detail on them. 2) GameOver text is always on

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -2,54 +2,102 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
-	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
+
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
+
+    public GameObject back;  // back of card;
+    public CardDefinition def;  // from DeckXML.xml		
+
+    public SpriteRenderer[] spriteRenderers;
+    void Start()
+    {
+        SetSortOrder(0);
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sORD)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sORD;
+                continue;
+            }
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sORD + 2;
+                    break;
+
+                case "face":
+                default:
+                    tSR.sortingOrder = sORD + 1;
+                    break;
+            }
+        }
+    }
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
+    }
 
 
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
-        f
-		set {
-			back.SetActive(!value);
-		}
-	}
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
-
-[System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+public class Decorator
+{
+    public string type;         // For card pips, tyhpe = "pip"
+    public Vector3 loc;         // location of sprite on the card
+    public bool flip = false;   //whether to flip vertically
+    public float scale = 1.0f;
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    public string face; //sprite to use for face cart
+    public int rank;    // value from 1-13 (Ace-King)
+    public List<Decorator>
+                    pips = new List<Decorator>();  // Pips Used
 }
